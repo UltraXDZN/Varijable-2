2f52707984e2cafc596c7867ee20abff9229f05d
UltraXDZN <57915175+UltraXDZN@users.noreply.github.com>
2020-11-09 09:09:28 +0100

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Varijable2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte sto = 100;
+            int tisucu = -1000;
+            char slovo = 'a';
+            string istina = "\"true\"";
+            float prviBroj = 3.123f;
+            double drugiBroj = 3.123456789;
+            bool istinaB = true;
+
+            Console.WriteLine($"{sto}, {tisucu}, {slovo}, {prviBroj}, {drugiBroj}, {istinaB}, {istina}");
+            Console.ReadKey();
+        }
+    }
+}
